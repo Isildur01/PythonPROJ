903ff534ff288aea07d52dff3bc16783277a8018
Leandro Santos <leandropsantos25@gmail.com>
2024-03-11 20:18:32 -0300

    11/03 20:h18

---
+++ interface_Tkinter.py
@@ -0,0 +1,13 @@
+from tkinter import *
+import time;
+import datetime
+import pygame, sys, random
+
+pygame.init()
+root = Tk()
+root.title('SISTEMA VENDAS')
+root.geometry('500x300')
+
+root.configure(bg= 'yellow')
+
+root.mainloop()
